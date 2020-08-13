f056ad927eb5524703e3b5950cde68ced8bd0f9f
Andrey Lozovoy <lozovoya@gmail.com>
2020-08-13 17:23:23 +0300

    final task1

--- cmd/importer/main.go
+++ cmd/importer/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/lozovoya/gohomework8_1/pkg/card"
-	"io"
 	"log"
 	"os"
 	"runtime/trace"
@@ -32,26 +31,6 @@ func execute(filename string) (err error) {
 
 	svc := card.NewService()
 	err = svc.ImportCSV(filename)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	file, err := os.Create("export2.csv")
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	defer func(c io.Closer) {
-		if cerr := file.Close(); cerr != nil {
-			log.Println(cerr)
-			if err == nil {
-				err = cerr
-			}
-		}
-	}(file)
-
-	err = svc.Export(file)
 	if err != nil {
 		log.Println(err)
 		return
--- pkg/card/card_test.go
+++ pkg/card/card_test.go
@@ -12,7 +12,12 @@ func TestMapRowToTransaction(t *testing.T) {
 		wantAmount int64
 		wantOwner  int
 	}{
-		// TODO: Add test cases.
+		{
+			name:       "normal",
+			args:       args{row: []string{"151801", "6040", "4"}},
+			wantAmount: 151801,
+			wantOwner:  4,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
